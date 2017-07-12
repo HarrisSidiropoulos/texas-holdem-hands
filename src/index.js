e025179da2334455e7c04e0644baec60a5c88732
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2017-07-12 20:58:52 +0300

    clean up

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-/* eslint-disable no-console, no-restricted-syntax, no-bitwise, no-param-reassign */
+/* eslint-disable no-bitwise, no-param-reassign */
 const ranks = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2'];
+const STRAIGHT_FLUSH = 'straight-flush';
+const FOUR_OF_A_KIND = 'four-of-a-kind';
+const FULL_HOUSE = 'full house';
+const FLUSH = 'flush';
+const STRAIGHT = 'straight';
+const THREE_OF_A_KIND = 'three-of-a-kind';
+const TWO_PAIR = 'two pair';
+const PAIR = 'pair';
+const NOTHING = 'nothing';
 
 const getCardChar = card => card.substr(0, card.length - 1);
 const getCardRank = card => ranks.indexOf(getCardChar(card));
@@ -22,6 +31,7 @@ function hasStraight(cards) {
   }
   return false;
 }
+
 function getAllSuits(cards) {
   return Object.values(
     cards.reduce((obj, card) => {
@@ -55,6 +65,22 @@ function getPairRanks(cards, pairs, num = 1) {
   return [...p, ...c.slice(0, subtract)];
 }
 
+function getHandType(totalSuits, straight, totalPairs) {
+  const sPairs = totalPairs.slice().sort((a, b) => b.length - a.length);
+  if (totalSuits.length > 0) return (hasStraight(totalSuits[0])) ? STRAIGHT_FLUSH : FLUSH;
+  if (straight) return STRAIGHT;
+  if (totalPairs.length >= 1) {
+    if (totalPairs.length === 1) {
+      if (totalPairs[0].length === 2) return PAIR;
+      else if (totalPairs[0].length === 3) return THREE_OF_A_KIND;
+    }
+    if (sPairs[0].length === 4) return FOUR_OF_A_KIND;
+    if (sPairs[0].length === 2) return TWO_PAIR;
+    return FULL_HOUSE;
+  }
+  return NOTHING;
+}
+
 function hand(holeCards, communityCards) {
   const sortedCards = []
     .concat(holeCards, communityCards)
@@ -63,23 +89,26 @@ function hand(holeCards, communityCards) {
   const totalPairs = getAllPairs(sortedCards).filter(a => a.length > 1);
   const straight = hasStraight(sortedCards);
 
-  if (totalSuits.length > 0) {
-    const straightFlush = hasStraight(totalSuits[0]);
-    if (straightFlush) return { type: 'straight-flush', ranks: straightFlush.slice(0, 5) };
-    return { type: 'flush', ranks: totalSuits[0].map(c => getCardChar(c)).slice(0, 5) };
+  switch (getHandType(totalSuits, straight, totalPairs)) {
+    case STRAIGHT_FLUSH:
+      return { type: STRAIGHT_FLUSH, ranks: hasStraight(totalSuits[0]) };
+    case FOUR_OF_A_KIND:
+      return { type: FOUR_OF_A_KIND, ranks: getPairRanks(sortedCards, totalPairs).slice(0, 2) };
+    case FULL_HOUSE:
+      return { type: FULL_HOUSE, ranks: [getCardChar(totalPairs[0][0]), getCardChar(totalPairs[1][0])] };
+    case FLUSH:
+      return { type: FLUSH, ranks: totalSuits[0].map(c => getCardChar(c)).slice(0, 5) };
+    case STRAIGHT:
+      return { type: STRAIGHT, ranks: straight };
+    case THREE_OF_A_KIND:
+      return { type: THREE_OF_A_KIND, ranks: getPairRanks(sortedCards, totalPairs) };
+    case TWO_PAIR:
+      return { type: TWO_PAIR, ranks: getPairRanks(sortedCards, totalPairs, 2) };
+    case PAIR:
+      return { type: PAIR, ranks: getPairRanks(sortedCards, totalPairs) };
+    default:
+      return { type: NOTHING, ranks: sortedCards.slice(0, 5).map(c => getCardChar(c)) };
   }
-  if (straight) return { type: 'straight', ranks: straight };
-  if (totalPairs.length >= 1) {
-    const sPairs = totalPairs.slice().sort((a, b) => b.length - a.length);
-    if (totalPairs.length === 1) {
-      if (totalPairs[0].length === 2) return { type: 'pair', ranks: getPairRanks(sortedCards, totalPairs) };
-      else if (totalPairs[0].length === 3) return { type: 'three-of-a-kind', ranks: getPairRanks(sortedCards, totalPairs) };
-    }
-    if (sPairs[0].length === 4) return { type: 'four-of-a-kind', ranks: getPairRanks(sortedCards, totalPairs).slice(0, 2) };
-    if (sPairs[0].length === 2) return { type: 'two pair', ranks: getPairRanks(sortedCards, totalPairs, 2) };
-    return { type: 'full house', ranks: [getCardChar(totalPairs[0][0]), getCardChar(totalPairs[1][0])] };
-  }
-  return { type: 'nothing', ranks: sortedCards.slice(0, 5).map(c => getCardChar(c)) };
 }
 
 export default hand;
